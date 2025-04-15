003603dda59d32fa49c6b2ff4e2e4070d9d67473
nugurib <nuguri990717@gamil.com>
2025-04-16 04:37:02 +0800

    s7.实现分类管理查看分类

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,5 +1,10 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+
 from app.auth.views.auth import login_required
+from app.blog.models import Category
+
+
+
 
 bp = Blueprint('admin',__name__, url_prefix='/admin',
                static_folder='static', template_folder='templates')
@@ -9,5 +14,19 @@ bp = Blueprint('admin',__name__, url_prefix='/admin',
 @bp.route('/')
 @login_required
 def index():
-   
     return render_template('admin/index.html')
+
+
+@bp.route('/category')
+@login_required
+def category():
+    # 查看分类
+    page = request.args.get('page', 1, type=int)
+    pagination = Category.query.order_by(
+        -Category.add_date).paginate(page=page, per_page=10, error_out=False)
+    category_list = pagination.items
+    return render_template(
+        'admin/category.html', 
+        category_list=category_list, 
+        pagination=pagination
+    )
