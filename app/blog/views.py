0186a356c4e21b057515cda136205c9763dd46a5
nugurib <nuguri990717@gamil.com>
2025-04-19 06:54:40 +0800

    s15.实现分类列表，文章详情页和完善文章url

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,6 +1,6 @@
 from flask import(Blueprint, render_template, request, 
                   redirect, url_for, flash, session)
-from .models import Post
+from .models import Post, Category, Tag
 
 
 bp = Blueprint('blog',__name__, url_prefix='/blog',
@@ -31,3 +31,31 @@ def index():
 
     # posts = [1,2,3,4,5,6,]
     return render_template('index.html', posts=post_list, pagination=pagination)
+
+#文章列表页
+@bp.route('/category/<int:cate_id>')
+def category(cate_id):
+    """
+    博客分类视图函数
+    """
+    cate =Category.query.get(cate_id)
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.filter(Post.category_id==cate_id).paginate(page=page, per_page=2, error_out=False)
+    post_list = pagination.items
+    return render_template('cate_list.html', cate=cate, post_list=post_list, 
+                            cate_id=cate_id, pagination=pagination)    
+
+
+#文章详情页
+@bp.route('/category/<int:cate_id>/<int:post_id>', methods=['GET', 'POST'])
+def detail(cate_id, post_id):
+    """
+    博客文章详情视图函数
+    """
+    cate = Category.query.get(cate_id)
+    post = Post.query.get_or_404(post_id)
+    #上一篇
+    prev_post = Post.query.filter(Post.id < post.id).order_by(-Post.id).first() 
+    #下一篇
+    next_post = Post.query.filter(Post.id > post.id).order_by(Post.id).first()
+    return render_template('detail.html', cate=cate, post=post,prev_post=prev_post, next_post=next_post)                            
